8fb5197e5af83a9a656365a5ef7c49506dfa027d
Tim Wade <hello@timjwade.com>
2017-12-04 11:54:34 -0800

    Add a basic http client

--- lib/manageiq/graphql/client/ruby.rb
+++ lib/manageiq/graphql/client/ruby.rb
@@ -1,10 +1,14 @@
 require "manageiq/graphql/client/ruby/version"
+require "graphql/client"
+require "graphql/client/http"
 
 module Manageiq
   module Graphql
     module Client
       module Ruby
-        # Your code goes here...
+        HTTP = GraphQL::Client::HTTP.new("http://localhost:3000/graphql")
+        Schema = GraphQL::Client.load_schema(File.expand_path(File.join("..", "..", "..", "..", "config", "schema.json"), __dir__))
+        Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
       end
     end
   end
