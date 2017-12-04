08bbaff2531319b4e8c3bdd7b163a0a7be97aef1
Tim Wade <hello@timjwade.com>
2017-12-04 11:21:43 -0800

    New rubygem

---
+++ lib/manageiq/graphql/client/ruby.rb
@@ -0,0 +1,11 @@
+require "manageiq/graphql/client/ruby/version"
+
+module Manageiq
+  module Graphql
+    module Client
+      module Ruby
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/manageiq/graphql/client/ruby/version.rb
@@ -0,0 +1,9 @@
+module Manageiq
+  module Graphql
+    module Client
+      module Ruby
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
