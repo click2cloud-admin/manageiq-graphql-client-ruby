4cfd4c0c5220ac662c477a60ba2f72721b35beb3
Tim Wade <hello@timjwade.com>
2017-12-06 15:48:37 -0800

    Fetch a remotely hosted queries repository

--- lib/manageiq/graphql.rb
+++ lib/manageiq/graphql.rb
@@ -1,6 +1,8 @@
 require "manageiq/graphql/version"
 require "graphql/client"
 require "graphql/client/http"
+require "net/http"
+require "json"
 
 module Manageiq
   module Graphql
@@ -8,5 +10,25 @@ module Manageiq
     Schema = GraphQL::Client.load_schema(File.expand_path(File.join("..", "..", "config", "schema.json"), __dir__))
     Client = GraphQL::Client.new(schema: Schema, execute: HTTP)
 
+    class QueryAdapter
+      URL = "http://localhost:3001/graphql/queries".freeze
+
+      def self.all
+        @all ||= JSON.parse(Net::HTTP.get(URI(URL)))["data"].each_with_object({}) do |query, result|
+          const_set(query["operationName"], Client.parse(query["query"]))
+          result[query["operationName"]] = const_get(query["operationName"]).const_get(query["operationName"])
+        end
+      end
+    end
+
+    class QueryRepository
+      def self.all
+        QueryAdapter.all.values
+      end
+
+      def self.find(name)
+        QueryAdapter.all.fetch(name)
+      end
+    end
   end
 end
