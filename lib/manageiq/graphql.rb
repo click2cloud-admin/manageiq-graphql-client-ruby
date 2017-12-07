7013d320d70f9676901486dca66bde6e6d675900
Tim Wade <hello@timjwade.com>
2017-12-06 16:02:17 -0800

    Add a few comments

--- lib/manageiq/graphql.rb
+++ lib/manageiq/graphql.rb
@@ -15,7 +15,14 @@ module Manageiq
 
       def self.all
         @all ||= JSON.parse(Net::HTTP.get(URI(URL)))["data"].each_with_object({}) do |query, result|
+          # The graphql-client implementation requires that this is
+          # assigned to a constant
           const_set(query["operationName"], Client.parse(query["query"]))
+
+          # We are only interested in the operation definition portion
+          # of the document (this is the thing we have to send to
+          # `.query`), hence the somewhat awkward constant lookup
+          # here.
           result[query["operationName"]] = const_get(query["operationName"]).const_get(query["operationName"])
         end
       end
